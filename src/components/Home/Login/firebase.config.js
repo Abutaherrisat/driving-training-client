6dfcb14563fcdb64ac578312b5a3307168388464
Abutaherrisat <abutaherrisat@gmail.com>
2021-12-16 20:13:04 +0600

    done

--- src/components/Home/Login/firebase.config.js
+++ src/components/Home/Login/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBHvC47gIy1Wk9OsC3-5AGGygPqm7kc4fQ",
-  authDomain: "driving-training-c9c42.firebaseapp.com",
-  projectId: "driving-training-c9c42",
-  storageBucket: "driving-training-c9c42.appspot.com",
-  messagingSenderId: "807108994629",
-  appId: "1:807108994629:web:0fae5aebaab4078fa9a52c"
+  apiKey: "AIzaSyBOIkbdFqLzcIC4uS51a_xnS3itwR5PdlY",
+  authDomain: "driving-training-4f0a8.firebaseapp.com",
+  projectId: "driving-training-4f0a8",
+  storageBucket: "driving-training-4f0a8.appspot.com",
+  messagingSenderId: "258110035616",
+  appId: "1:258110035616:web:f5fca6571f003e08ef9bc5"
 };
   export default firebaseConfig;
